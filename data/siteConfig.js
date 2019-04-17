9fd65f3e6a0d909cd9f2cc3e6c49fd4fc69c2f80
miron4dev <emironen0@gmail.com>
2019-04-18 00:18:28 +0300

    Initial commit

---
+++ data/siteConfig.js
@@ -0,0 +1,144 @@
+module.exports = {
+  siteTitle: 'Hi! I\'m Evgeny!',
+  siteDescription: `Evgeny Mironenko CV`,
+  keyWords: ['mironenko', 'evgeny', 'evgeny mironenko'],
+  authorName: 'Evgeny Mironenko',
+  twitterUsername: 'miron4dev',
+  githubUsername: 'miron4dev',
+  authorAvatar: '/images/avatar.jpeg',
+  authorDescription: `I have spent the last six years developing custom software solutions. 
+I believe that good software takes the commitment of customers, users and technology specialists. 
+I love what I'm doing because it allows build something that matters, build something that solves a problem.
+I enjoy being challenged with projects that require me to work outside my comfort and knowledge set.`,
+  skills: [
+    {
+      name: 'Java',
+      level: 100,
+    },
+    {
+      name: 'Java EE',
+      level: 100,
+    },
+    {
+      name: 'Spring',
+      level: 100,
+    },
+    {
+      name: 'Kotlin',
+      level: 100,
+    },
+    {
+      name: 'Ruby',
+      level: 100,
+    },
+    {
+      name: 'SOAP',
+      level: 100,
+    },
+    {
+      name: 'REST',
+      level: 100,
+    },
+    {
+      name: 'XML',
+      level: 100,
+    },
+    {
+      name: 'SQL',
+      level: 100,
+    },
+    {
+      name: 'JSF',
+      level: 100,
+    },
+    {
+      name: 'JavaScript',
+      level: 100,
+    },
+    {
+      name: 'React',
+      level: 100,
+    },
+    {
+      name: 'Vue',
+      level: 100,
+    },
+  ],
+  jobs: [
+    {
+      company: 'GrabCAD',
+      begin: {
+        month: 'mar',
+        year: '2019',
+      },
+      duration: null,
+      occupation: 'Senior Software Engineer',
+      description: 'Designing, developing and supporting innovative solutions for 3D printers and also for one of the largest online community of professional engineers, designers and manufacturers',
+
+    }, {
+      company: 'Omniva',
+      begin: {
+        month: 'may',
+        year: '2018',
+      },
+      duration: '1 yr',
+      occupation: 'Fullstack developer',
+      description: `
+      • developing business logic for the largest sorting line in the Baltic countries<br/>
+      • active participating in launch of a few new projects <br/>
+      • support and troubleshooting of Estonian sorting center <br/>
+      `,
+    }, {
+      company: 'T-Systems Russia',
+      begin: {
+        month: 'feb',
+        year: '2017',
+      },
+      duration: '1 yr 3 mos',
+      occupation: 'Senior Software Engineer',
+      description: `
+      • technical project leading an internal project for the largest telecommunications provider in Europe<br/>
+      • supervise the whole development cycle, from concept to delivery, including architecture, testing and so on<br/>
+      • prototyping, development and integration in close cooperation with Product Owner<br/>
+      • mentoring less experienced team members`,
+    }, {
+      company: 'T-Systems Russia',
+      begin: {
+        month: 'jun',
+        year: '2014',
+      },
+      duration: '2 yrs 2 mos',
+      occupation: 'Software Engineer'
+    },{
+      company: 'T-Systems Russia',
+      begin: {
+        month: 'may',
+        year: '2013',
+      },
+      duration: '1 yr',
+      occupation: 'Junior Software Engineer'
+    },
+    /* ... */
+  ],
+  social: {
+    twitter: 'https://twitter.com/miron4dev',
+    linkedin: 'https://www.linkedin.com/in/miron4dev',
+    github: 'https://github.com/miron4dev',
+    email: 'miron4devf@gmail.com',
+    facebook: 'https://facebook.com/miron4dev',
+  },
+  siteUrl: 'https://miron4dev.com',
+  pathPrefix: '/', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpeg',
+  googleAnalyticsId: 'UA-131359385-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Evgeny Mironenko',
+      url: '/',
+    },
+  ],
+}
