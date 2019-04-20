90b200f83ad0ee9d46e39f3f6a37e591444d2769
miron4dev <emironen0@gmail.com>
2019-04-21 02:36:09 +0300

    Work page has been implemented

---
+++ src/components/Job.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+interface JobProps {
+  company: string,
+  date: string,
+  position: string,
+  passed?: boolean
+}
+
+class Job extends React.Component<JobProps, any> {
+
+  getCompanyClass = () => {
+    let className = "float-left";
+    if (this.props.passed) {
+      className += " text-muted";
+    }
+    return className;
+  };
+
+  getDateClass = () => {
+    let className = "float-right";
+    if (this.props.passed) {
+      className += " text-muted";
+    }
+    return className;
+  };
+
+  getPositionClass = () => {
+    let className = "card-title";
+    if (this.props.passed) {
+      className += " text-muted";
+    }
+    return className;
+  };
+
+  getBorderClass = () => {
+    if (this.props.passed) {
+      return "card border-secondary shadow";
+    }
+    return "card border-success shadow";
+  };
+
+  getBadgeClass = () => {
+    if (this.props.passed) {
+      return "badge badge-pill bg-secondary";
+    }
+    return "badge badge-pill bg-success border";
+  };
+
+  getSupportBadgeClass = () => {
+    let className = "col";
+    if (this.props.passed) {
+      className += " border-right";
+    }
+    return className;
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-auto text-center flex-column d-none d-sm-flex">
+          <div className="row h-50">
+            <div className={this.getSupportBadgeClass()}>&nbsp;</div>
+            <div className="col">&nbsp;</div>
+          </div>
+          <h5 className="m-2">
+            <span className={this.getBadgeClass()}>&nbsp;</span>
+          </h5>
+          <div className="row h-50">
+            <div className="col border-right">&nbsp;</div>
+            <div className="col">&nbsp;</div>
+          </div>
+        </div>
+        <div className="col py-2">
+          <div className={this.getBorderClass()}>
+            <div className="card-body">
+              <div className={this.getCompanyClass()}>{this.props.company}</div>
+              <div className={this.getDateClass()}>{this.props.date}</div>
+              <h4 className={this.getPositionClass()}>{this.props.position}</h4>
+
+              {this.props.children}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Job;
---
+++ src/pages/work.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Job from "../components/Job";
+import Navbar from "../components/Navbar";
+import Typist from "../components/Typist";
+import MainLayout from "../layouts";
+
+class WorkPage extends React.Component {
+  render() {
+    return (
+
+      <MainLayout>
+        <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
+          <Navbar/>
+          <div className="container-fluid" style={{ flex: 1 }}>
+            <main>
+              <section className="py-3 py-md-4">
+                <div className="d-flex align-items-end typist">
+                  <h2><Typist messages={["My experience"]}/></h2>
+                </div>
+              </section>
+
+              <div className="container py-2">
+                <Job company="GrabCAD" position="Senior Software Engineer" date="March 2019 - Ongoing">
+                  <p className="card-text">
+                    designing, developing and supporting innovative solutions for 3D printers and also for one of
+                    the largest online community of professional engineers, designers and manufacturers.
+                  </p>
+                </Job>
+
+                <Job company="Omniva" position="FullStack Engineer" date="May 2018 - February 2019"
+                     passed={true}>
+                  <ul className="card-text text-muted" style={{ textAlign: "left" }}>
+                    <li>
+                      developing business logic for the largest sorting line in the Baltic countries
+                    </li>
+                    <li>
+                      active participating in launch of a few new projects
+                    </li>
+                    <li>
+                      support and troubleshooting of Estonian sorting center
+                    </li>
+                  </ul>
+                </Job>
+
+                <Job company="T-Systems RUS" position="Senior Software Engineer" date="Feb 2017 - April 2018"
+                     passed={true}>
+                  <ul className="card-text text-muted" style={{ textAlign: "left" }}>
+                    <li>
+                      technical project leading an internal project for the largest telecommunications provider in
+                      Europe
+                    </li>
+                    <li>
+                      supervise the whole development cycle, from concept to delivery, including architecture, testing
+                      and so on
+                    </li>
+                    <li>
+                      prototyping, development and integration in close cooperation with Product Owner
+                    </li>
+                    <li>
+                      mentoring less experienced team members
+                    </li>
+                  </ul>
+                </Job>
+
+                <Job company="T-Systems RUS" position="Software Engineer" date="Jun 2014 - Feb 2017"
+                     passed={true}/>
+                <Job company="T-Systems RUS" position="Junior Software Engineer" date="Jun 2013 - Jun 2014"
+                     passed={true}/>
+              </div>
+            </main>
+          </div>
+        </div>
+      </MainLayout>
+    );
+  }
+}
+
+export default WorkPage;
